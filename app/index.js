0408c3f482f4215d43c8124cf5feae4915de48c7
Kira McLean <kiraemclean@gmail.com>
2016-09-11 11:02:15 -0400

    add avatar component

--- app/index.js
+++ app/index.js
@@ -1,16 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-export class HelloWorld extends React.Component  {
+var USER_DATA = {
+  name: "Kira McLean",
+  username: "kiramclean",
+  imageUrl: "https://avatars2.githubusercontent.com/u/11531673?v=3&s=460"
+}
+
+export class ProfilePic extends React.Component {
   render() {
-      console.log(this.props)
     return (
-      <section class='container'>HelloReCT!!!</section>
+      <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+    )
+  }
+}
+
+export class ProfileLink extends React.Component {
+  render() {
+    return (
+      <div>
+        <a href={`https://github.com/${this.props.username}`}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+}
+
+export class ProfileName extends React.Component {
+  render() {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+}
+
+export class Avatar extends React.Component {
+  render() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.imageUrl} />
+        <ProfileLink username={this.props.user.username} />
+        <ProfileName name={this.props.user.name} />
+      </div>
     )
   }
 }
 
 ReactDOM.render(
-  <HelloWorld name="Kira" otherData={30} />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 )
