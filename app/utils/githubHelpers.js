420946d45238ba495f27563a4d1dc3d419bf399d
Kira McLean <kiraemclean@gmail.com>
2016-09-14 07:34:47 -0400

    more github helpers

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -3,10 +3,6 @@ import axios from 'axios'
 let token = 'TOKEN'
 let param = `?access_token=${token}`
 
-function getUserInfo (username) {
-  return axios.get(`https://api.github.com/users/${username}${param}`)
-}
-
 const helpers = {
   getPlayersInfo(usernames) {
     return axios.all(usernames.map(function (username) {
@@ -20,28 +16,48 @@ const helpers = {
     })
   }
 
-  function getUserInfo (username) {
+  getUserInfo (username) {
     return axios.get(`https://api.github.com/users/${username}${param}`)
   }
 
   battle() {
+    let playerOneData = getPlayersData(player[0])
+    let playerTwoData = getPlayersData(player[1])
 
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(funciton(error) {
+        console.warn('Error in getPlayersInfo: ', error)
+      })
   }
+
+  getPlayersData(player) {
+    return getRepos(player.login)
+      .then(getTotalStars)
+      .then(function(totalStars) {
+        return {
+          followers: player.followers,
+          totalStars: totalStars
+        }
+      })
+  }
+
 
   getRepos(username) {
     return axios.get(`https://api.github.com/users/${username}/repos/${param}&per_page=100`)
   }
 
   getTotalStars(repos) {
-
-  }
-
-  getPlayersData(player) {
-
+    return repos.data.reduce(function(prev, current) {
+      return prev + current.stargazers_count
+    }, 0)
   }
 
   calculateScores(players) {
-
+    return [
+      players[0].followers * 3 + players[0].totalStars,
+      players[1].followers * 3 + players[1].totalStars,
+    ]
   }
 }
 
